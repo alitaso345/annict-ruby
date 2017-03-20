288223cf54e3fc0be833ab52b9b9967735cfcedb
alitaso345 <a.maruyama@uec.ac.jp>
2017-03-20 23:56:11 +0900

    Add api methods sending get method

--- lib/annict/api_methods.rb
+++ lib/annict/api_methods.rb
@@ -3,5 +3,45 @@ module Annict
     def works(params = {})
       send_get("/v1/works", params)
     end
+
+    def episodes(params = {})
+      send_get("/v1/episodes", params)
+    end
+
+    def records(params = {})
+      send_get("/v1/records", params)
+    end
+
+    def users(params = {})
+      send_get("/v1/users", params)
+    end
+
+    def following(params = {})
+      send_get("/v1/following", params)
+    end
+
+    def followers(params = {})
+      send_get("/v1/followers", params)
+    end
+
+    def activities(params = {})
+      send_get("/v1/activities", params)
+    end
+
+    def me(params = {})
+      send_get("/v1/me", params)
+    end
+
+    def own_works(params = {})
+      send_get("/v1/me/works", params)
+    end
+
+    def programs(params = {})
+      send_get("/v1/me/programs", params)
+    end
+
+    def following_activities(params = {})
+      send_get("/v1/me/following_activities", params)
+    end
   end
 end
